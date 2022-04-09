1f4e281bc48d209faeb194667218dd54b555e575
aycadogan89 <doganayca89@gmail.com>
2022-04-09 01:48:22 -0700

    cors error message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,21 @@
+import { Card,Tab,Tabs} from '@blueprintjs/core'
+import { useState } from 'react'
+import Login from './pages/Login'
+import Register from './pages/Register'
 
 function App() {
+
+  const [currentTab, setCurrentTab] = useState('login')
+
+
   return (
     <div className="App">
-      
+        <Card>
+            <Tabs id='Tabs' onChange={setCurrentTab} selectedTabId={currentTab}>
+              <Tab id='login' title='Login' panel={<Login />} />
+              <Tab id='register' title="Register" panel={<Register />} />
+            </Tabs>
+        </Card>    
     </div>
   );
 }
